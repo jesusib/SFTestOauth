153337fd75155d427f857069eb9d5a4059a8427a
jesus.ib <jesus.igleber@gmail.com>
2017-04-04 16:00:54 +0200

    flask installed

---
+++ app.py
@@ -0,0 +1,21 @@
+'''
+Created on 4 abr. 2017
+
+@author: jeiglesias
+'''
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key ='random string'
+
+@app.route('/')
+def index():
+    return 'hello SF'
+
+if __name__ == '__main__':
+    Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    
+   #pp.debug = True
+   #app.run()
